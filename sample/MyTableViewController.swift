d3f0f6efc4e5be1d97f486aa5b8565356136bfea
Ni Ryogo <niryogo@enuainoiMac.local>
2020-06-29 09:35:37 +0900

    cell reomve animation OK

--- sample/MyTableViewController.swift
+++ sample/MyTableViewController.swift
@@ -12,6 +12,26 @@ class MyTableViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     
+    var dataSource:[String] = [
+        "a",
+        "a",
+        "a",
+        "a",
+        "a",
+        "abb",
+        "abb",
+        "abb"
+    ]
+    var secondDataSource:[String] = [
+        "a",
+        "a",
+        "a",
+        "a",
+        "a",
+        "abb",
+        "abb",
+        "abb"
+    ]
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tableView.delegate = self
@@ -21,8 +41,24 @@ class MyTableViewController: UIViewController {
 }
 
 extension MyTableViewController:UITableViewDelegate,UITableViewDataSource{
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 50
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
         return 2
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return self.dataSource.count
+        }
+        if section == 1 {
+            return self.secondDataSource.count
+        }
+        return 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -38,5 +74,16 @@ extension MyTableViewController:UITableViewDelegate,UITableViewDataSource{
         return 50
     }
     
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == UITableViewCell.EditingStyle.delete {
+            if indexPath.section == 0 {
+                self.dataSource.remove(at: indexPath.row)
+            }
+            if indexPath.section == 1 {
+                self.secondDataSource.remove(at: indexPath.row)
+            }
+            self.tableView.deleteRows(at: [indexPath as IndexPath], with: UITableView.RowAnimation.automatic)
+        }
+    }
     
 }
--- sample/NavTableViewController.swift
+++ sample/NavTableViewController.swift
@@ -12,6 +12,16 @@ class NavTableViewController: UITableViewController {
     
     @IBOutlet weak var searchBar: UISearchBar!
     
+    var dataSource:[String] = [
+        "a",
+        "a",
+        "a",
+        "a",
+        "a",
+        "abb",
+        "abb",
+        "abb"
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,12 +32,19 @@ class NavTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 10
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return self.dataSource.count
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == UITableViewCell.EditingStyle.delete {
+            self.dataSource.remove(at: indexPath.row)
+            self.tableView.deleteRows(at: [indexPath as IndexPath], with: UITableView.RowAnimation.automatic)
+        }
     }
 }
 
